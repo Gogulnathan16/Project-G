155ffadba16f2d83c854a9eb63dd7a387fa6a382
Gogulnathan16 <gokulfaraday@gmail.com>
2020-09-06 22:29:25 +0530

    okay

--- src/org/com/Z.java
+++ src/org/com/Z.java
@@ -3,12 +3,11 @@ package org.com;
 public class Z {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		int x1=8;
-=======
 		int x=9;
 		int y=15;
->>>>>>> 3ebc29c82da3fcef358829a18d7778c6ddc0f725
+		int x1=8;
+
+
 	}
 
 }
